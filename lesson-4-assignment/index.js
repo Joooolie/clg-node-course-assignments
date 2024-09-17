9e07f174969556162daa3dc0026b44b75e32949d
Julie Zenou <me@juliezen.com>
2024-09-17 11:24:08 +1000

    assignment 4

---
+++ lesson-4-assignment/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.get('/', (req,res) => {
+    res.send("Hello World");
+})
+
+app.get('/firstServer/', (req,res) => {
+    res.send("Hi There! This is my first route built using express framework");
+})
+
+app.get('/firstUser:id/', (req,res) => {
+    res.send("Hi there, your user ID is ...");
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
