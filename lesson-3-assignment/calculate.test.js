ad0eafdd1789ba8e0c2c3bd6ad2381e2a0a912e1
Joooolie <me@juliezen.com>
2024-09-23 21:22:44 +1000

    assignment 3 set up

---
+++ lesson-3-assignment/calculate.test.js
@@ -0,0 +1,11 @@
+const mathOperations = require('./calculator');
+
+// Write a test for adding 2 numbers and validate the expected results
+
+// Write a failing test and see what output you get
+
+// Write more test cases: for subtract and multiply
+
+// Run the test in your terminal
+
+// Observe the test results and coverage from your test cases
