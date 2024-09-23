2788a995a0c36d95263ad40385674b8bbafc3229
Joooolie <me@juliezen.com>
2024-09-23 22:39:14 +1000

    clean up of code

--- lesson-5-assignment/index.js
+++ lesson-5-assignment/index.js
@@ -8,14 +8,12 @@ const weddingSinger = {
 // Given an array of movies with title and storyline, write a function getMovies() that iterates 
 // through the movies list but the loop is enclosed within a setTimeout of 1 second (1000 ms)
 function getMovies(movies) {
-    //if (typeof(movies) != Array) return;
-
     setTimeout(() => {
         try {
             movies.forEach(movie => {
                 console.log(`Title: ${movie.title}, Description: ${movie.description}`);
             });
-        return movies;
+            return movies;
         } catch(err) {
             console.log(err.message);
         }
@@ -35,10 +33,9 @@ function createMovies(movie, movies) {
 }
 
 function addMovie(movie, movies) {
-    //if (typeof(movies) != Array) return;
     try {
         movies.push(movie);
-    return movies;
+        return movies;
     } catch(err) {
         console.log(err.message);
     }
@@ -48,7 +45,6 @@ function addMovie(movie, movies) {
 // Write an init() function that calls createMovies() and getMovies() but 
 // getMovies() will only run after createMovies() is done.
 async function init(movie, movies) {
-    console.log("start");
     await createMovies(movie, movies);
     getMovies(movies);
 }
