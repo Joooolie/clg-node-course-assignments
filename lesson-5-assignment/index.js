b07df5d11842fc0dc9a1b53651d0c2d53940ea40
Joooolie <me@juliezen.com>
2024-09-23 22:15:42 +1000

    assignment 5

---
+++ lesson-5-assignment/index.js
@@ -0,0 +1,58 @@
+const romComs = require('./romComs');
+
+const weddingSinger = {
+    title : "The Wedding Singer", 
+    description : "Robbie, a singer, and Julia, a waitress, are both engaged, but to the wrong people. Fortune intervenes to help them discover each other."
+}
+
+// Given an array of movies with title and storyline, write a function getMovies() that iterates 
+// through the movies list but the loop is enclosed within a setTimeout of 1 second (1000 ms)
+function getMovies(movies) {
+    //if (typeof(movies) != Array) return;
+
+    setTimeout(() => {
+        try {
+            movies.forEach(movie => {
+                console.log(`Title: ${movie.title}, Description: ${movie.description}`);
+            });
+        return movies;
+        } catch(err) {
+            console.log(err.message);
+        }
+    }, 1000);
+}
+
+
+// Add another function called createMovies(). This function fetches an argument movie and pushes it to the array list.
+// But the function createMovies() returns a promise and contains a timeout of 2 secs (2000 ms).
+function createMovies(movie, movies) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(addMovie(movie, movies))
+        }, 2000);
+    })
+    
+}
+
+function addMovie(movie, movies) {
+    //if (typeof(movies) != Array) return;
+    try {
+        movies.push(movie);
+    return movies;
+    } catch(err) {
+        console.log(err.message);
+    }
+    
+}
+
+// Write an init() function that calls createMovies() and getMovies() but 
+// getMovies() will only run after createMovies() is done.
+async function init(movie, movies) {
+    console.log("start");
+    await createMovies(movie, movies);
+    getMovies(movies);
+}
+
+init(weddingSinger, romComs);
+
+// Also include error handling if possible.
---
+++ lesson-5-assignment/romComs.js
@@ -0,0 +1,20 @@
+const romComs = [{
+    title: "Our Family Wedding",
+    description: "The weeks leading up to a young couple's wedding are comic and stressful, especially as their respective fathers try to lay their long standing feud to rest."
+}, {
+    title: "Valentine's Day",
+    description: "Intertwining couples and singles in Los Angeles break-up and make-up based on the pressures and expectations of Valentine's Day."
+}, {
+    title: "Serendipity",
+    description: "A couple search for each other years after the night they first met, fell in love, and separated, convinced that one day they'd end up together."
+}, {
+    title: "He's Just Not That Into You",
+    description: "This Baltimore-set movie of interconnecting story arcs deals with the challenges of reading or misreading human behavior."
+}, {
+    title: "The Bounty Hunter",
+    description: "A bounty hunter learns that his next target is his ex-wife, a reporter working on a murder cover-up. Soon after their reunion, the always-at-odds duo find themselves on a run-for-their-lives adventure."
+}
+
+];
+
+module.exports = romComs;
